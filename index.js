d37287f4d94431fcc94256c73d8c43f61854a648
Chenesyy <chesternevansy@gmail.com>
2022-11-06 20:02:04 +0800

    Added methods and consoles

---
+++ index.js
@@ -0,0 +1,23 @@
+const sum = (x, y) => {
+    return x + y;
+}
+
+const difference = (x, y) => {
+    return x - y;
+}
+
+const product = (x, y) => {
+    return x * y;
+}
+
+const quotient = (x, y) => {
+    return x / y;
+}
+
+let a = 1;
+let b = 2
+
+console.log(`Sum: ${sum(a, b)}`);
+console.log(`Difference: ${difference(a, b)}`);
+console.log(`Product: ${product(a, b)}`);
+console.log(`Quotient: ${quotient(a, b)}`);
